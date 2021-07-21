89c27b8cf521195481febd1c7b7c14e4578a4104
Terry Chen <radiant107@gmail.com>
2021-07-22 00:13:29 +0800

    calculate version

--- t.py
+++ t.py
@@ -4,8 +4,15 @@ with open('reviews.txt', 'r') as a:
 	for line in a:
 		data.append(line.strip())
 		count += 1
-		if count % 1000 == 0:
+		if count % 100000 == 0: # count和100000去求餘數為0
 			print(len(data))
-print(data[0])
+
+print('檔案讀取完了，總共有', len(data),'筆資料')
+
+
 print('-----------')
-print(data[2])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言平均長度是', sum_len/len(data))

b819d32feefde8eb024c6e5b5a578dc9bcf44996
Terry Chen <radiant107@gmail.com>
2021-07-21 23:37:43 +0800

    count version

--- t.py
+++ t.py
@@ -1,1 +1,11 @@
-gits
+data = []
+count = 0
+with open('reviews.txt', 'r') as a:
+	for line in a:
+		data.append(line.strip())
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print(data[0])
+print('-----------')
+print(data[2])
